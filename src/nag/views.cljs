5ea73d90b139389696e1e921af59946a69de362e
Nolan330 <nolan@nuid.io>
2019-12-02 16:35:05 -0500

    Refactored scroll listener, nag.views formatting tweaks

--- src/nag/views.cljs
+++ src/nag/views.cljs
@@ -1,59 +1,73 @@
 (ns nag.views
   (:require
+   [nag.isotope :as isotope]
+   [reagent.core :as r]
    [goog.events.EventType :as EventType]
    [goog.events :as events]
-   [goog.dom :as dom]
-   [reagent.core :as r]
-   [nag.isotope :as isotope]))
+   [goog.dom :as dom]))
 
 (defn default-header-container
   [{:keys [title-component nav-component]}]
   (let [dropdown? (r/atom false)
-        scroll    (r/atom 0)]
+        scroll    (r/atom 0)
+        listener  (r/atom nil)]
+    (r/create-class
+     {:component-did-mount
+      (fn []
+        (->>
+         (events/listen
+          js/window
+          EventType/SCROLL
+          (fn [] (reset! scroll (max 0 (.-y (dom/getDocumentScroll))))))
+         (reset! listener)))
 
-    (events/listen
-     js/window
-     EventType/SCROLL
-     (fn []
-       (reset! scroll (max 0 (.-y (dom/getDocumentScroll))))))
+      :component-will-unmount
+      (fn []
+        (events/unlistenByKey @listener)
+        (reset! dropdown? false)
+        (reset! scroll 0)
+        (reset! listener nil))
 
-    (fn []
-      [:div#header-container
-       {:class (when (> @scroll 0) "pinned")
-        :style {:background isotope/gray1-rgb
-                :position   (when (> @scroll 3000) "fixed")}}
+      :reagent-render
+      (fn []
+        [:div#header-container
+         {:class (when (> @scroll 0) "pinned")
+          :style {:background @isotope/gray1-rgb-css-str-ratom
+                  :position   (when (> @scroll 3000) "fixed")}}
 
-       [:div#header
-        {:style {:color isotope/gray2-rgb}}
+         [:div#header
+          {:style {:color @isotope/gray2-rgb-css-str-ratom}}
 
-        [title-component]
+          [title-component]
 
-        (when nav-component
-          [:div
-           [:button.mobile-only
+          (when nav-component
+            [:div
+             [:button.mobile-only
+              {:class    (when @dropdown? "active")
+               :style    {:color @isotope/gray2-rgb-css-str-ratom}
+               :on-click (fn [] (reset! dropdown? (not @dropdown?)))}
+              "•."]
+
+             [nav-component {:class "desktop-only"}]])]
+
+         (when nav-component
+           [:div#header-dropdown
             {:class    (when @dropdown? "active")
-             :style    {:color isotope/gray2-rgb}
+             :style    {:background @isotope/gray1-rgb-css-str-ratom
+                        :color      @isotope/gray2-rgb-css-str-ratom}
              :on-click (fn [] (reset! dropdown? (not @dropdown?)))}
-            "•."]
-
-           [nav-component {:class "desktop-only"}]])]
-
-       (when nav-component
-         [:div#header-dropdown
-          {:class    (when @dropdown? "active")
-           :style    {:background isotope/gray1-rgb :color isotope/gray2-rgb}
-           :on-click (fn [] (reset! dropdown? (not @dropdown?)))}
-          [nav-component]])])))
+            [nav-component]])])})))
 
 (defn nav
   [props]
   [:ul props
-   (for [{:keys [display on-click]} isotope/nav-list]
-     ^{:key display}
-     [:li
-      {:on-click on-click
-       :style    {:color isotope/gray2-rgb}}
-      display])])
+   (doall
+    (for [{:keys [display on-click]} isotope/nav-list]
+      ^{:key display}
+      [:li
+       {:on-click on-click
+        :style    {:color @isotope/gray2-rgb-css-str-ratom}}
+       display]))])
 
 (defn header
   []
@@ -64,6 +78,7 @@
 (defn content
   []
   [:div.isotope-container
+   {:style {:background @isotope/gray1-rgb-css-str-ratom}}
    [:div.isotope-sizer]
    (for [i isotope/isotopes]
      ^{:key (:id i)}
@@ -72,5 +87,6 @@
 (defn main-panel
   []
   [:div
+   {:style {:background @isotope/gray1-rgb-css-str-ratom}}
    [header]
    [content]])
