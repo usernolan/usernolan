b5372ad851456d6e396ee8da760dcbbdc54763ac
Nolan330 <nolan@nuid.io>
2020-04-05 22:05:52 -0400

    Error handling

--- src/nag/core.cljs
+++ src/nag/core.cljs
@@ -1,5 +1,8 @@
 (ns nag.core
   (:require
+   [goog.array :as arr]
+   [goog.dom :as dom]
+   [goog.userAgent :as userAgent]
    [nag.isotope :as isotope]
    [nag.views :as views]
    [reagent.dom :as r.dom]))
@@ -10,7 +13,23 @@
    [views/main-panel]
    (js/document.getElementById "app")))
 
+(defn error!
+  [message]
+  (doto (arr/peek (dom/getElementsByTagName dom/TagName.BODY))
+    (dom/setProperties  #js {"style" "padding:5vh 5vw;"})
+    (dom/append (doto (dom/createElement "h1")
+                  (dom/setTextContent message)))))
+
+(defn ie!
+  []
+  (error! "don't use internet explorer."))
+
 (defn ^:export init
   []
-  (render)
-  (isotope/init))
+  (if userAgent/IE
+    (ie!)
+    (try
+      (render)
+      (isotope/init)
+      (catch :default _
+        (error! "something went heinously wrong.")))))
