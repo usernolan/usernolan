c73cd75cdaa8c7537d3a9ff65030f28ed4818cf5
usernms <nolan@nuid.io>
2022-10-28 17:22:32 -0400

    Sketch nav components

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,85 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import { $compile } from "@thi.ng/rdom/compile"
+import { $klist } from "@thi.ng/rdom/klist"
+import { reactive } from "@thi.ng/rstream/stream"
+import { sync } from "@thi.ng/rstream/sync"
+import { EVENT_ROUTE_CHANGED, HTMLRouter } from "@thi.ng/router"
 
-interface SvgHrefParams {
-  href: string;
-  src: string;
-  cls: string;
-  alt: string;
+const routes = [
+  { id: "default", match: ["gist"] },
+  { id: "gist", match: ["?who", "gist"] },
+  { id: "gallery", match: ["?who", "gallery"] },
+  { id: "gallery-item", match: ["?who", "gallery", "?gid"] },
+  { id: "refs", match: ["?who", "refs"] },
+  { id: "quotes", match: ["?who", "quotes"] }
+]
+// {id: "generic", match: ["?who", "?what"]} ...
+// {id: "nolan-gist", match: ["nolan", "gist"]} ...
+
+const routerConfig = {
+  useFragment: true,
+  defaultRouteID: routes[0].id,
+  routes: routes
 }
 
-const svgHref = ({ href, src, cls, alt }: SvgHrefParams) => {
-  return `
-  <a href="${href}" target="_blank">
-    <img src="${src}" class="${cls}" alt="${alt}" />
-  </a>`
-};
+const router = new HTMLRouter(routerConfig)
+router.start()
 
-const viteSvg = svgHref({
-  href: "https://vitejs.dev",
-  src: "/vite.svg",
-  cls: "logo",
-  alt: "Vite logo"
-});
+const who = reactive(router.current?.params?.who || "nolan")
+const what = reactive(router.current?.id.split("-")[0] || "gist")
 
-const typescriptSvg = svgHref({
-  href: "https://www.typescriptlang.org/",
-  src: typescriptLogo,
-  cls: "logo vanilla",
-  alt: "TypeScript logo"
-});
+const page = sync({ src: { who, what } })
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    ${viteSvg}
-    ${typescriptSvg}
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const whoAll = ["nolan", "nm8", "Oe", "smixzy"]
+const whatAll = ["gist", "gallery", "refs", "quotes"]
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const whoElse = who.map((x) => whoAll.filter((y) => x != y))
+const whatElse = what.map((x) => whatAll.filter((y) => x != y))
+
+router.addListener(EVENT_ROUTE_CHANGED, (e) => {
+  console.log(e)
+  who.next(e?.value?.params?.who || "nolan")
+  what.next(e?.value?.id.split("-")[0] || "gist")
+})
+
+const whoHovered = reactive(false);
+const whoClicked = reactive(false);
+const whatHovered = reactive(false);
+const whatClicked = reactive(false);
+
+const whoNav = sync({ src: { whoHovered, whoClicked } })
+const whatNav = sync({ src: { whatHovered, whatClicked } })
+
+const app = $compile([
+  "div", { class: page.map((x) => `rdom-root ${x.who} ${x.what}`) },
+  ["div.nav-container", {},
+    ["nav",
+      {
+        class: whoNav.map((x) => x.whoHovered || x.whoClicked ? "expanded" : ""),
+        onmouseenter: () => whoHovered.next(true),
+        onmouseleave: () => whoHovered.next(false)
+      },
+      ["button", { onclick: () => whoClicked.next(!whoClicked.deref()) }, who],
+      $klist(
+        whoElse,
+        "ul",
+        {},
+        (x) => ["li", {}, ["a", { href: what.map((y) => `/#/${x}/${y}`) }, x]],
+        (x) => x)],
+    ["nav",
+      {
+        class: whatNav.map((x) => x.whatHovered || x.whatClicked ? "expanded" : ""),
+        onmouseenter: () => whatHovered.next(true),
+        onmouseleave: () => whatHovered.next(false)
+      },
+      ["button", { onclick: () => whatClicked.next(!whatClicked.deref()) }, what],
+      $klist(
+        whatElse,
+        "ul",
+        {},
+        (x) => ["li", {}, ["a", { href: who.map((y) => `/#/${y}/${x}`) }, x]],
+        (x) => x)],],
+  ["main", {}, page.map((x) => `${x.who}/${x.what}`)]
+])
+
+app.mount(document.getElementById("app")!)
