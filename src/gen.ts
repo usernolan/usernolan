9f937ed4e9d10485c775da0ee3be3f8f44fe004b
usernolan <nolan@nuid.io>
2023-01-19 11:52:34 -0500

    Format html in version control and dev

--- src/gen.ts
+++ src/gen.ts
@@ -1,8 +1,15 @@
 import { writeFileSync } from "node:fs"
+import { argv } from "node:process"
 import { serialize } from "@thi.ng/hiccup"
 import { document } from "./markup.js" // NOTE: ts-node-esm requires .js ext
+import beautify from "js-beautify" // NOTE: named import unsupported
 
 
 /* NOTE: generate index */
 
-writeFileSync("index.html", serialize(document))
+const raw = serialize(document)
+
+const data = argv.find((x) => x === '--release') ? raw
+  : beautify.html_beautify(raw, { indent_size: 2 })
+
+writeFileSync("index.html", data)
