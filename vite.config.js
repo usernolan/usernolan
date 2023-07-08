9b8142a29162c90398b4df20bc598e6115c3d1dc
usernolan <nolan@usernolan.net>
2023-07-07 22:32:47 -0400

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
+        index: resolve(__dirname, 'index.html'),
         images: resolve(__dirname, 'images/index.html'),
         links: resolve(__dirname, 'links/index.html'),
         quotes: resolve(__dirname, 'quotes/index.html')
